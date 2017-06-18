b5544295737da7a7fa0095770e2319b2dfb0c80a
Jijo Bose <bosejijo@gmail.com>
2017-06-18 15:37:57 +0530

    bootstrap layout and menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'home_pages/home'
+
+  get 'home_pages/about'
+
+  root 'home_pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
