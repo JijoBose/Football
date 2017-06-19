ba433e317eeefe436b9a4922e4483bd3df5de181
Jijo Bose <bosejijo@gmail.com>
2017-06-19 12:11:12 +0530

    improved UI

--- app/helpers/home_pages_helper.rb
+++ app/helpers/home_pages_helper.rb
@@ -1,2 +1,10 @@
 module HomePagesHelper
+  def upcomming_match
+    Match.where(:schedule => Time.now..6.months.after)
+  end
+
+  def team_name_finder(getid)
+    a = Team.where(id: getid).pluck(:name)
+    return a[0]
+  end
 end
--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -3,4 +3,9 @@ module TeamsHelper
     Team.select(:name, :id).map{|c| [c.name, c.id] }
     #Team.all.collect { |m| [m.id, m.name] }
   end
+
+  def player_sort_list(teamid)
+    Player.all.where(id: teamid)
+    #Team.all.collect { |m| [m.id, m.name] }
+  end
 end
