9d80a20d1c5d3e0afe5fcdfc26590941e6574025
Jijo Bose <bosejijo@gmail.com>
2017-06-19 13:17:48 +0530

    Added helper modules for query

--- app/helpers/home_pages_helper.rb
+++ app/helpers/home_pages_helper.rb
@@ -7,4 +7,19 @@ module HomePagesHelper
     a = Team.where(id: getid).pluck(:name)
     return a[0]
   end
+
+  def find_captain(matchid)
+    b = Role.where(match_id: matchid).where(captain: true).pluck(:player_id)
+    temp = b[0]
+    final = Player.where(id: temp).pluck(:name)
+    return final[0]
+  end
+
+  def find_goalkeeper(matchid)
+    b = Role.where(match_id: matchid).where(goalkeeper: true).pluck(:player_id)
+    temp = b[0]
+    final = Player.where(id: temp).pluck(:name)
+    tfinal = final[0]
+    return tfinal
+  end
 end
